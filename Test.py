44ee4924c53588a83935d7315cede11d55eed25e
gayathri7022 <gayathria7022@gmail.com>
2025-04-15 10:32:05 +0530

    Adding files

--- Test.py
+++ Test.py
@@ -5,3 +5,5 @@ def Test_cases():
     assert is_prime(2) == "2 is a prime number"
     assert is_prime(-1) == "numbers lesser than 2 are not prime numbers."
     assert is_prime(9) == "not a prime"
+
+Test_cases()

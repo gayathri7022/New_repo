335cb029b0ae4fd91a41da497f74275a9fefa65e
gayathri7022 <gayathria7022@gmail.com>
2025-04-15 10:45:20 +0530

    Adding files

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
 from Prime import is_prime
 
 def Test_cases():
-    assert is_prime(1) == False
-    assert is_prime(2) == True
-    assert is_prime(-1) == False
-    assert is_prime(9) == True
+    assert(is_prime(1)) == False
+    assert(is_prime(2)) == True
+    assert(is_prime(-1)) == False
+    assert(is_prime(9)) == True
 

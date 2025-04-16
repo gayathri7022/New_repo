b1dc38a8247c627560ca5de87c480538c07f162d
gayathri7022 <gayathria7022@gmail.com>
2025-04-16 11:30:46 +0530

    adding modified files

--- Prime.py
+++ Prime.py
@@ -5,13 +5,13 @@ def is_prime(num):
     if num == 2:
         return True
     
-    is_prime = True
+    is__prime = True
     for i in range(2,int(num/2)+1,1):
         if num%i == 0:
-            is_prime = False
+            is__prime = False
             break
 
-    if is_prime:
+    if is__prime:
         return True
     else:
         return False
